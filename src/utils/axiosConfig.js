19b6fec05c373a4ad4a3e6416d88264abf93085e
amjadali070 <amjadpitafi070@gmail.com>
2024-11-06 23:36:38 +0500

    adding env

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 
 // Create an Axios instance with default configurations
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   withCredentials: true, // Send cookies with requests
 });
+
+console.log("baseURL", process.env.REACT_APP_API_BASE_URL)
 
 // Optional: Add interceptors for request and response if needed
 axiosInstance.interceptors.request.use(
