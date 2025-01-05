c7727c69ca3dde87ba6d26a5fa73ada929fc128d
amjadali070 <amjadpitafi070@gmail.com>
2025-01-05 15:21:28 +0500

    Clear cache

--- src/Components/molecules/Sidebar.tsx
+++ src/Components/molecules/Sidebar.tsx
@@ -100,7 +100,7 @@ const menuItems: MenuItem[] = [
       //   visibleTo: ["SuperAdmin"],
       // },
       {
-        label: "Password",
+        label: "Password Manager",
         path: "/admin/password-manager",
         visibleTo: ["SuperAdmin"],
       },
